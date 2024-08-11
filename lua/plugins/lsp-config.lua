865236212b21e56d56ca977dac20dcdf39721a8a
valrover <dental-pushes.6s@icloud.com>
2024-08-12 01:26:16 +0200

    new feats: ident helps and minor fixes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,21 +1,55 @@
 return {
     {
         "williamboman/mason.nvim",
-        lazy = false,
+        --lazy = false,
+        dependencies = {
+            "WhoIsSethDaniel/mason-tool-installer.nvim"
+        },
         config = function()
-            require("mason").setup()
+            local mason = require("mason")
+
+            local mason_tool_installer = require("mason-tool-installer")
+
+            mason.setup()
+
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    "stylua",
+                    "clang-format",
+                    "yapf",
+                    "yamlfix"
+
+                }
+            })
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        lazy = false,
+        --lazy = false,
         opts = {
             auto_install = true,
         },
+        config = function ()
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "clangd",
+                    "pyright",
+                },
+            })
+        end
     },
     {
         "neovim/nvim-lspconfig",
-        lazy = false,
+        --lazy = false,
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            { "antosha417/nvim-lsp-file-operations", config = true },
+            { "folke/neodev.nvim", opts = {} },
+        },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
