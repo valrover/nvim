f6485cbcd052bb2e9a8610ad79a5e2731de51b73
valrover <dental-pushes.6s@icloud.com>
2024-08-12 01:26:16 +0200

    new feats: ident helps and minor fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,11 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
         build = ":TSUpdate",
+        dependencies = {
+            "windwp/nvim-ts-autotag"
+        },
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = {
@@ -24,6 +28,7 @@ return {
                 auto_install = true,
                 highlight = { enable = true },
                 indent = { enable = true },
+                autotag = { enable = true },
             })
         end,
     },
