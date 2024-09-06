a5b235e3c1d3972648ba1a8123e9eec1ca5d98e2
valrover <dental-pushes.6s@icloud.com>
2024-09-06 22:25:46 +0200

    feat: lualine!

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,8 +15,8 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<D-f>", builtin.find_files, {})
-      vim.keymap.set("n", "<D-g>", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+      vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
       require("telescope").load_extension("ui-select")
