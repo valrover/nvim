78e418a655fd343e8a10996c9bd4596ba00fe1eb
valrover <dental-pushes.6s@icloud.com>
2025-01-20 04:40:56 +0100

    feat: xml syntax highlighting for ROS launch files

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
+-- Lazy config
 require("config.lazy")
+
+-- ROS config
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.launch",
+    callback = function()
+        vim.bo.filetype = "xml"
+    end,
+})
