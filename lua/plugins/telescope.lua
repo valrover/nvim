38f433ed8234e000057a5b4b4a4bc0718ce740a0
jose-conbotics <jose.valdivieso@conbotics.com>
2025-06-04 23:31:16 +0200

    improve some keybindings and get rid of some warnings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,6 +18,9 @@ return {
       vim.keymap.set("n", "<C-f>", builtin.find_files, {})
       vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      vim.keymap.set("n", "<C-w>", builtin.diagnostics, {})
+      vim.keymap.set("n", "C-i", builtin.lsp_implementations, {})
+      vim.keymap.set("n", "C-d", builtin.lsp_definitions, {})
 
       require("telescope").load_extension("ui-select")
     end,
