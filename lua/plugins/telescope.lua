34f1b514759bff848d516ee4667f1759e8130fe7
valrover <josevaldivieso98@gmail.com>
2025-06-14 19:59:37 +0200

    feat: default mappings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,12 +15,9 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-f>", builtin.find_files, {})
-      vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<C-w>", builtin.diagnostics, {})
-      vim.keymap.set("n", "C-i", builtin.lsp_implementations, {})
-      vim.keymap.set("n", "C-d", builtin.lsp_definitions, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc = 'Telescope find files'})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = 'Telescope live grep'})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
 
       require("telescope").load_extension("ui-select")
     end,
