942f451fe18c59b26b9b741521a33ae4025cb515
valrover <dental-pushes.6s@icloud.com>
2025-08-29 17:16:17 +0200

    fix: git modified filed in orange now

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,7 @@ return {
             vim.cmd.colorscheme("oxocarbon")
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#FFA500" })
         end,
     },
 }
