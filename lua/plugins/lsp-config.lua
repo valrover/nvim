da67a42f4a42aa9f9fd799a90d600793c7d2d0e8
valrover <josevaldivieso98@gmail.com>
2025-09-18 12:55:51 +0200

    fix: lspconfig deprecated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,84 +1,67 @@
+-- Updated LSP configuration using `vim.lsp.enable`
 return {
     {
         "williamboman/mason.nvim",
-        --lazy = false,
         dependencies = {
             "WhoIsSethDaniel/mason-tool-installer.nvim"
         },
         config = function()
-            local mason = require("mason")
-
-            local mason_tool_installer = require("mason-tool-installer")
-
-            mason.setup()
-
-            mason_tool_installer.setup({
-                ensure_installed = { "clang-format",
-                    "stylua",
-                    "ruff" }
+            require("mason").setup()
+            require("mason-tool-installer").setup({
+                ensure_installed = { "clang-format", "stylua", "ruff" }
             })
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        --lazy = false,
         opts = {
             auto_install = true,
         },
         config = function()
-            local mason_lspconfig = require("mason-lspconfig")
-
-            mason_lspconfig.setup({
-                ensure_installed = { "clangd",
-                    "lua_ls",
-                    "pylsp" },
-
-                automatic_enable = {
-                    exclude = {"clangd",
-                        "lua_ls",
-                        "pylsp" },
-                }
+            require("mason-lspconfig").setup({
+                ensure_installed = { "clangd", "lua_ls", "pylsp" },
+                automatic_enable = { exclude = { "clangd", "lua_ls", "pylsp" } },
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
-        --lazy = false,
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             { "antosha417/nvim-lsp-file-operations", config = true },
-            { "folke/neodev.nvim",                   opts = {} },
+            { "folke/neodev.nvim", opts = {} },
         },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities.offsetEncoding = "utf-8"
 
-            capabilities.offsetEncoding = 'utf-8'
+            -- Use vim.lsp.enable instead of require('lspconfig').<server>.setup
+            vim.lsp.enable("clangd", {
+                capabilities = capabilities,
+                cmd = { "clangd", "--completion-style=bundled", "--header-insertion=never" },
+            })
 
-            local lspconfig = require("lspconfig")
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                cmd = { "clangd", "--completion-style=bundled", "--header-insertion=never" }, -- Disable auto-includes
-            })
-            lspconfig.lua_ls.setup({
+            vim.lsp.enable("lua_ls", {
                 capabilities = capabilities,
             })
-            lspconfig.pylsp.setup({
+
+            vim.lsp.enable("pylsp", {
                 capabilities = capabilities,
                 settings = {
                     pylsp = {
                         plugins = {
-                            pycodestyle = {
-                                ignore = { "E501" , "W503"},
-                            }
+                            pycodestyle = { ignore = { "E501", "W503" } },
                         },
                         signature = {
-                            formatter = 'ruff',
-                            ignore = { "E501" , "W503"}
-                        }
-                    }
-                }
+                            formatter = "ruff",
+                            ignore = { "E501", "W503" },
+                        },
+                    },
+                },
             })
+
+            -- Key mappings and diagnostic configurations
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
@@ -87,7 +70,7 @@ return {
             vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
             vim.diagnostic.config({ virtual_text = false })
             vim.o.updatetime = 250
-            vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+            vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
         end,
     },
 }
